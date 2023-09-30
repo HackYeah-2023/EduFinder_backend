9173671d78fa186a421924ef33b26cd13e9f5bfa
Piotr Bukowiec <pbuk60@gmail.com>
2023-09-30 12:47:46 +0200

    Nie działa SQL w pliku

--- src/routes/schools-routes.ts
+++ src/routes/schools-routes.ts
@@ -1,11 +1,17 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import {getDb} from '../database/connection';
 
 const schoolsRouter = express.Router();
-schoolsRouter.get('/', async (_req, res) => {
-    const db = await getDb();
-    const data = await db.execute('SELECT * FROM SCHOOLS LIMIT 300');
-    res.json(data[0]);
+schoolsRouter.get('/', async (req: Request, res) => {
+	const {name} = req.query;
+	console.log(name);
+	const db = await getDb();
+	const data = await db.execute(
+		`SELECT * FROM SCHOOLS WHERE ${name ? 'name LIKE %123%' : ''} LIMIT 300`,
+		req.query
+	);
+	res.json(data[0]);
 });
 
 export default schoolsRouter;
+//TODO: opcjonalne filrowanie po: city, county (powiat), type (rodzaj szkoły)
