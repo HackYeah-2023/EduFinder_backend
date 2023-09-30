8366debd48b851841498b1b975a64ba54ab8d920
Pawel-Kica <pawel.kica.cc@proton.me>
2023-09-30 12:51:40 +0200

    New: name

--- src/routes/schools-routes.ts
+++ src/routes/schools-routes.ts
@@ -3,14 +3,13 @@ import {getDb} from '../database/connection';
 
 const schoolsRouter = express.Router();
 schoolsRouter.get('/', async (req: Request, res) => {
-	const {name} = req.query;
-	console.log(name);
-	const db = await getDb();
-	const data = await db.execute(
-		`SELECT * FROM SCHOOLS WHERE ${name ? 'name LIKE %123%' : ''} LIMIT 300`,
-		req.query
-	);
-	res.json(data[0]);
+    const {name} = req.query;
+    const db = await getDb();
+    const query = `SELECT * FROM SCHOOLS WHERE ${name ? 'name LIKE :name' : ''} LIMIT 300`;
+    const data = await db.execute(query, {
+        name: name ? `%${name}%` : null,
+    });
+    res.json(data[0]);
 });
 
 export default schoolsRouter;
